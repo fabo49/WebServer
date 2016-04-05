a0285b7219842379b54f356587843cd448582269
Fabian Rodriguez <farodriguez.49@gmail.com>
2016-04-05 14:14:14 -0600

    Creación de los hilos

--- Server.py
+++ Server.py
@@ -1,27 +1,70 @@
-import socket
+import socket, time, thread
 
-# --------------Conexion entrante-----------------
-# Creando el socket TCP/IP
-sock_input = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Variable que comparten todos los hilos
+log = open("log.csv", "w")  # Donde se va a guardar la bitacora del programa
+log.write("Metodo,Estampilla de tiempo,Servidor,Refiere,URL,Datos\n")
 
-# Enlace de socket y puerto conexion entrante
-server_address = ('localhost', 1080)
-sock_input.bind(server_address)
+server_port = 2080  # Puerto de escucha del servidor
 
-# Escuchando conexiones entrantes
-sock_input.listen(10)
-input_connection, client_address = sock_input.accept()
 
-data_buffer = {}
-data_size = 10000
+# ---- Funciones del servidor ----
+def Get(data):
+    print("Entre a GET")
 
-while len(data_buffer) < data_size <= 10000:
 
-    try:
-        data = input_connection.recv(1000).decode()
-        print(data)
+def Post(data):
+    print("Entre a POST")
 
-    except:
-        break
 
-sock_input.close()
+def Head(data):
+    print("Entre a HEAD")
+
+
+def ProcessData(thread_number, data):
+    print("Soy el hilo {}".format(thread_number))
+    print(data)
+    # Obtiene identifica el tipo de operacion y la ejecuta
+    operation = data.split('\n')[0].split(' ')[0]
+    if (operation == "GET"):
+        Get(data)
+    elif (operation == "POST"):
+        Post(data)
+    elif (operation == "HEAD"):
+        Head(data)
+
+
+def OpenServer():
+    # --------------Conexion entrante-----------------
+    # Creando el socket TCP/IP
+    sock_input = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Enlace de socket y puerto conexion entrante
+    server_address = ('localhost', server_port)
+    sock_input.bind(server_address)
+
+    # Escuchando conexiones entrantes
+    sock_input.listen(10)
+    input_connection, client_address = sock_input.accept()
+
+    data_buffer = {}
+    data_size = 10000
+    thread_number = 1
+    while len(data_buffer) < data_size <= 10000:
+
+        # TODO: hacer los hilos
+        try:
+            data = input_connection.recv(1000).decode()
+            # Revisa que tenga datos
+            if data:
+                thread.start_new_thread(ProcessData, (thread_number, data))
+                thread_number += 1
+        except:
+            print("ERROR: no se pudo establecer conexion con el socket")
+            break
+
+    sock_input.close()
+
+
+# ----- Programa (main)-----
+
+OpenServer()
