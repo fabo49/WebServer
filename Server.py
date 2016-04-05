0c49e6b3b810ca0a9d411697ebb6531951eaca59
carlos.mataguzman <carlos.mataguzman@ucr.ac.cr>
2016-04-04 19:45:15 -0600

    Se crea un socket para las conexiones y se levanta el servidor

---
+++ Server.py
@@ -0,0 +1,27 @@
+import socket
+
+#--------------Conexion entrante-----------------
+# Creando el socket TCP/IP
+sock_input = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Enlace de socket y puerto conexion entrante
+server_address = ('localhost', 2080)
+sock_input.bind(server_address)
+
+# Escuchando conexiones entrantes
+sock_input.listen(10)
+input_connection, client_address = sock_input.accept()
+
+data_buffer = {}
+data_size = 10000
+
+
+while len(data_buffer) < data_size <= 10000:
+
+    try:
+        data = input_connection.recv(1000).decode()
+        print(data)
+
+    except:
+        sock_input.close()
+        break
